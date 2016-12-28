2926bbf84a1861df56ef3a9293613e87d491ee06
Anna Agafonova <pups.ann@gmail.com>
2016-12-28 13:50:58 +0100

    Update

--- src/test/java/basic/Hooks.java
+++ src/test/java/basic/Hooks.java
@@ -1,7 +1,10 @@
 package basic;
 
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import cucumber.api.java.Before;
 
@@ -13,11 +16,11 @@ public class Hooks {
 	public void setup() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "/Users/anna/Documents/workspace/testio/chromedriver");
 //		Jenkins runs
-//		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-//		driver = new RemoteWebDriver(new URL("http://10.252.47.15:5555/wd/hub"), capabilities);
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		driver = new RemoteWebDriver(new URL("http://10.252.47.15:5555/wd/hub"), capabilities);
 		
 		
 //		Local runs
-		driver = new ChromeDriver();
+//		driver = new ChromeDriver();
 	}
 }
