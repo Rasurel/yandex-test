cdd7c5b8e34d2cb450565949f0bd6329491b60e9
Anna Agafonova <anna@Annas-MacBook-Pro.local>
2016-12-28 10:10:35 +0100

    Settings update

--- src/test/java/basic/Hooks.java
+++ src/test/java/basic/Hooks.java
@@ -1,7 +1,10 @@
 package basic;
 
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import cucumber.api.java.Before;
 
@@ -12,9 +15,9 @@ public class Hooks {
 	@Before
 	public void setup() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "/Users/anna/Documents/workspace/testio/chromedriver");
-		//DesiredCapabilities dc = new DesiredCapabilities();
-		//dc.setBrowserName("chrome");
-		//driver = new RemoteWebDriver(new URL("http://10.252.47.15:4444/wd/hub"), dc);
-		driver = new ChromeDriver();
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		driver = new RemoteWebDriver(new URL("http://10.252.47.15:4444/wd/hub"), dc);
+		//driver = new ChromeDriver();
 	}
 }
